5befdef8ec21f575316cc7f26e2281a506bea3e6
Renan Tokashiki <renantoka@icloud.com>
2021-07-08 12:28:07 -0400

    base do front end

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import DataTable from "components/DataTable";
+import BarChart from "components/BarChart";
+import DonutChart from "components/DonutChart";
+import Routes from "Routes";
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
